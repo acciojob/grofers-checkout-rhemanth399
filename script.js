9b000916fe475a1e8243a52189058101128a669d
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-02-18 08:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let prices=document.querySelectorAll('.price');
 
 const getSum = () => {
 //Add your code here
+let totalPrice=0;
+  prices.forEach(price=>{
+	  totalPrice+=parseFloat(price.textContent);
+      const table = document.querySelector('table');
+  const newRow = table.insertRow();
+  const newCell = newRow.insertCell();
   
+  // Set the text content of the total price cell to the calculated total price
+  newCell.textContent = `Total Price: $${totalPrice}`;
+
+  })
 };
+
 
 getSumBtn.addEventListener("click", getSum);
 
